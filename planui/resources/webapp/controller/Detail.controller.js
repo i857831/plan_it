878325525fbe0e7c417a18fb73b161694c351f9d
C5229696 C5229696 <anastasiya.khvat@sap.com>
2017-05-26 10:49:14 +0000

    UI improvements

--- planui/resources/webapp/controller/Detail.controller.js
+++ planui/resources/webapp/controller/Detail.controller.js
@@ -4,19 +4,14 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"plan/model/formatter",
 	"plan/model/dateFormatter"
-
 ], function(BaseController, JSONModel, formatter, dateFormatter) {
 	"use strict";
-
 	return BaseController.extend("plan.controller.Detail", {
-
 		formatter: formatter,
 		dateFormatter: dateFormatter,
-
 		/* =========================================================== */
 		/* lifecycle methods                                           */
 		/* =========================================================== */
-
 		onInit: function() {
 			// Model used to manipulate control states. The chosen values make sure,
 			// detail page is busy indication immediately so there is no break in
@@ -25,22 +20,16 @@ sap.ui.define([
 				busy: false,
 				delay: 0
 			});
-
 			this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
-
 			this.setModel(oViewModel, "detailView");
-
 			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
 		},
-
 		/* =========================================================== */
 		/* event handlers                                              */
 		/* =========================================================== */
-
 		/* =========================================================== */
 		/* begin: internal methods                                     */
 		/* =========================================================== */
-
 		/**
 		 * Binds the view to the object path and expands the aggregated line items.
 		 * @function
@@ -56,7 +45,6 @@ sap.ui.define([
 				this._bindView("/" + sObjectPath);
 			}.bind(this));
 		},
-
 		/**
 		 * Binds the view to the object path. Makes sure that detail view displays
 		 * a busy indicator while data for the corresponding element binding is loaded.
@@ -67,10 +55,8 @@ sap.ui.define([
 		_bindView: function(sObjectPath) {
 			// Set busy indicator during view binding
 			var oViewModel = this.getModel("detailView");
-
 			// If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
 			oViewModel.setProperty("/busy", false);
-
 			this.getView().bindElement({
 				path: sObjectPath,
 				events: {
@@ -84,11 +70,9 @@ sap.ui.define([
 				}
 			});
 		},
-
 		_onBindingChange: function() {
 			var oView = this.getView(),
 				oElementBinding = oView.getElementBinding();
-
 			// No data for the binding
 			if (!oElementBinding.getBoundContext()) {
 				this.getRouter().getTargets().display("detailObjectNotFound");
@@ -97,29 +81,26 @@ sap.ui.define([
 				this.getOwnerComponent().oListSelector.clearMasterListSelection();
 				return;
 			}
-
 			var sPath = decodeURI(oElementBinding.getPath()),
 				oResourceBundle = this.getResourceBundle(),
 				oObject = oView.getModel().getObject(sPath),
 				sObjectId = oObject.id,
 				sObjectName = oObject.description,
 				oViewModel = this.getModel("detailView");
-
 			this.getOwnerComponent().oListSelector.selectAListItem(sPath);
-
 			oViewModel.setProperty("/saveAsTileTitle", oResourceBundle.getText("shareSaveTileAppTitle", [sObjectName]));
 			oViewModel.setProperty("/shareOnJamTitle", sObjectName);
-			oViewModel.setProperty("/shareSendEmailSubject",
-				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-			oViewModel.setProperty("/shareSendEmailMessage",
-				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+			oViewModel.setProperty("/shareSendEmailSubject", oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+			oViewModel.setProperty("/shareSendEmailMessage", oResourceBundle.getText("shareSendEmailObjectMessage", [
+				sObjectName,
+				sObjectId,
+				location.href
+			]));
 		},
-
 		_onPlAttrSelected: function(oEvent) {
 			var that = this;
 			var object = oEvent.getSource().getSelectedItem().getBindingContext().getObject();
 			var oTable = this.getView().byId("__table1");
-
 			var oTemplate = new sap.m.ColumnListItem({
 				cells: [
 					new sap.m.Text({
@@ -133,7 +114,6 @@ sap.ui.define([
 					})
 				]
 			});
-
 			oTable.removeAllItems();
 			//oTable.addItem(oTemplate);                     
 			var sPath = "PlAttr(" + object.ID + ")/Effort";
@@ -141,9 +121,7 @@ sap.ui.define([
 				path: sPath,
 				template: oTemplate
 			});
-
 		},
-
 		_onEffortChange: function(oEvent) {
 			var oModel = this.getView().getModel();
 			var newEffort = oEvent.getParameter("value");
@@ -151,7 +129,6 @@ sap.ui.define([
 			var oContext = oEvent.getSource().getParent().getBindingContext();
 			var itemObject = oContext.getObject();
 			itemObject.effort = newEffort;
-
 			updateModel.update(oContext.sPath, itemObject, {
 				async: false,
 				success: function(oData, response) {
@@ -162,12 +139,9 @@ sap.ui.define([
 				error: function(oError) {
 					jQuery.sap.require("sap.m.MessageBox");
 					sap.m.MessageBox.alert("Error during effort update");
-
 				}
 			});
-
 		},
-
 		_onCurrencyChange: function(oEvent) {
 			var oModel = this.getView().getModel();
 			var newCurrency = oEvent.getParameter("value");
@@ -180,7 +154,6 @@ sap.ui.define([
 			Version.startDate = itemObject.startDate;
 			Version.endDate = itemObject.endDate;
 			Version.currency = newCurrency;
-
 			updateModel.update(oContext.sPath, Version, {
 				async: false,
 				success: function(oData, response) {
@@ -191,53 +164,42 @@ sap.ui.define([
 				error: function(oError) {
 					jQuery.sap.require("sap.m.MessageBox");
 					sap.m.MessageBox.alert("Error during currency update");
-
 				}
 			});
-
 		},
-
 		_onMetadataLoaded: function() {
 			// Store original busy indicator delay for the detail view
 			var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
 				oViewModel = this.getModel("detailView");
-
 			// Make sure busy indicator is displayed immediately when
 			// detail view is displayed for the first time
 			oViewModel.setProperty("/delay", 0);
-
 			// Binding the view will set it to not busy - so the view is always busy if it is not bound
 			oViewModel.setProperty("/busy", true);
 			// Restore original busy indicator delay for the detail view
 			oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
 		},
-
 		/**
 		 *@memberOf plan.controller.Detail
 		 */
 		onOpenDialog: function(oEvent) {
-
 			var object = oEvent.getSource().getBindingContext().getObject();
 			var oModel = new sap.ui.model.json.JSONModel();
 			oModel.setData(object);
 			var oView = this.getView();
 			var oDialog = oView.byId("travelDetailsDialog");
-
 			if (!oDialog) {
 				// create dialog via fragment factory
 				oDialog = sap.ui.xmlfragment(oView.getId(), "plan.view.TravelDetailsDialog", this);
 				oView.addDependent(oDialog);
 				oDialog.setModel(oModel);
 			}
-
 			oDialog.setModel(oModel);
 			oDialog.open(); //This code was generated by the layout editor.
-
 		},
 		onCloseDialog: function() {
 			this.getView().byId("travelDetailsDialog").close();
-
 		}
-	});
 
+	});
 });
--- planui/resources/webapp/model/formatter.js
+++ planui/resources/webapp/model/formatter.js
@@ -1,23 +1,46 @@
-sap.ui.define([
-	], function () {
-		"use strict";
+sap.ui.define([], function() {
+	"use strict";
 
-		return {
-			/**
-			 * Rounds the currency value to 2 digits
-			 *
-			 * @public
-			 * @param {string} sValue value to be formatted
-			 * @returns {string} formatted currency value with 2 digits
-			 */
-			currencyValue : function (sValue) {
-				if (!sValue) {
-					return "";
-				}
-
-				return parseFloat(sValue).toFixed(2);
+	return {
+		/**
+		 * Rounds the currency value to 2 digits
+		 *
+		 * @public
+		 * @param {string} sValue value to be formatted
+		 * @returns {string} formatted currency value with 2 digits
+		 */
+		currencyValue: function(sValue) {
+			if (!sValue) {
+				return "";
 			}
-		};
 
-	}
-);
+			return parseFloat(sValue).toFixed(2);
+		},
+
+		textFormat: function(sValue) {
+			var sFormattedValue;
+			switch (sValue) {
+				case "P":
+					sFormattedValue = "Percentage";
+					break;
+				case "A":
+					sFormattedValue = "Absolute";
+					break;
+				case "NOREV":
+					sFormattedValue = "No Travel Revenue";
+					break;
+				case "LSUMD":
+					sFormattedValue = "Lump Sum a Day";
+					break;
+					case "REIMB":
+					sFormattedValue = "Expense Reimbursement (%)";
+					break;
+					
+									default:
+				sFormattedValue =	 sValue;
+			}
+			return sFormattedValue;
+		}
+
+	};
+});
